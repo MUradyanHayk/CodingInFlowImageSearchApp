11c8c23c95d908669989259c8817d8d914755f18
MUradyan <hayk.muradyan@zangi.com>
2023-04-27 10:16:55 +0400

    (HM) is added back button an do something

--- app/src/main/java/com/example/codinginflowimagesearchapp/screens/DetailFragment.kt
+++ app/src/main/java/com/example/codinginflowimagesearchapp/screens/DetailFragment.kt
@@ -1,11 +1,19 @@
 package com.example.codinginflowimagesearchapp.screens
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.codinginflowimagesearchapp.R
 import com.example.codinginflowimagesearchapp.databinding.FragmentDetailBinding
 import com.example.codinginflowimagesearchapp.model.UnsplashPhotoResult
@@ -32,6 +40,32 @@ class DetailFragment : Fragment() {
         Glide.with(requireContext())
             .load(_currentPhoto.urls?.regular)
             .centerCrop()
+            .error(R.drawable.ic_error)
+            .listener(object :RequestListener<Drawable>{
+                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                    binding.progressBar.isVisible = false
+                    return false
+                }
+
+                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    binding.progressBar.isVisible = false
+                    binding.textViewCreator.isVisible = true
+                    binding.textViewDescription.isVisible = _currentPhoto.description != null
+                    return false
+                }
+
+            })
             .into(binding.imageView)
+
+        binding.textViewDescription.text = _currentPhoto.description?:""
+        binding.textViewCreator.text = "Photo by ${_currentPhoto.user?.name ?: ""} on Unsplash"
+
+
+        val uri = Uri.parse(_currentPhoto.user?.attributionUrl)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        binding.textViewCreator.paint.isUnderlineText = true
+        binding.textViewCreator.setOnClickListener {
+            context?.startActivity(intent)
+        }
     }
 }
